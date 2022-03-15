7e2cfc89193d439f23ade93c9b2262a4cc75aecb
Raffi Hakhverdyan <haxverdyanraffi@mail.ru>
2022-03-15 15:03:43 +0400

    start --for android part

---
+++ src/rh/company/Used__HttpURLConnection.java
@@ -0,0 +1,24 @@
+package rh.company;
+
+
+public class Used__HttpURLConnection {
+
+    static boolean Send_Http_Request__GetResponse = true;
+    static boolean Send_Http_Request_Params__Text = true;
+    static boolean Send_Http_Request_Params__File = true;
+
+    public static void main(String[] args) {
+        if (Send_Http_Request__GetResponse) {
+
+        }
+        if (Send_Http_Request_Params__Text) {
+
+        }
+        if (Send_Http_Request_Params__File) {
+
+        }
+
+    }
+
+
+}
